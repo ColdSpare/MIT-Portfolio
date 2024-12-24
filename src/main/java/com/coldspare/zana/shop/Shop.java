4a84858a9ce752e224dc88d6ada0cb4d4db498f1
ColdSpare <113843399+ColdSpare@users.noreply.github.com>
2024-12-23 23:01:23 -0600

    Delete src/main/java/com/coldspare/zana/shop directory
    Removed the 'shop' folder, as it contained commented-out code and was not being used in the current project.

--- src/main/java/com/coldspare/zana/shop/Shop.java
+++
@@ -1,9 +0,0 @@
-/*package com.coldspare.zana;
-
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface Shop {
-    void openInventory(Player player);
-    void onInventoryClick(InventoryClickEvent e);
-}*/
